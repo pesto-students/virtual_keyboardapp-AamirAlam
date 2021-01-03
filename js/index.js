10fd9f1d588f5c807696c0e0a7dc302c666cd65c
Aamir Alam <aamiralam1991@gmail.com>
2021-01-03 23:17:25 +0530

    Add capsLock and tab actions in virtual keyboard

--- js/index.js
+++ js/index.js
@@ -11,6 +11,8 @@ inputKeys.addEventListener('click', handleInput)
 
 // actions
 
+// let CAPS_LOCK = false;
+
 function handleInput(event) {
 
     const item = event.target;
@@ -21,7 +23,12 @@ function handleInput(event) {
     }else if(item_class.includes('backspace')){
         backspaceAction()
     }else if(item_class.includes('enter')){
+        //todo
         enterAction()
+    }else if(item_class.includes('caps')){
+        toggleCapsLock(item);
+    }else if(item_class.includes('tab')){
+        insertText(item.innerText)
     }
 
    
@@ -34,6 +41,8 @@ function insertText(text) {
     }
     if (text == '_____space____'){
         textArea.innerText = textArea.value + " ";
+    }else if(text == 'tab'){
+        textArea.innerText = textArea.value + "        "
     }else{
         textArea.innerText = textArea.value + text;
     }
@@ -43,11 +52,12 @@ function insertText(text) {
     
     let start = textArea.selectionStart
     let end = textArea.selectionEnd;
-    end=  end+ textArea.value.length;
-    textArea.setSelectionRange(start, end)
+    // end=  end+ textArea.value.length;
+    // textArea.setSelectionRange(start, end)
     
     // console.log(start , textArea.selectionEnd)
-    textArea.focus()   
+    textArea.focus();
+    textArea.selectionEnd = end+ textArea.value.length;   
    
 }
 
@@ -57,8 +67,40 @@ function backspaceAction() {
     }
     current_text = textArea.value;
     textArea.innerText = current_text.slice(0,-1)
+
+    let start = textArea.selectionStart
+    let end = textArea.selectionEnd;
+    // end=  end+ textArea.value.length;
+    // textArea.setSelectionRange(start, end)
+    
+    // console.log(start , textArea.selectionEnd)
+    textArea.focus();
+    textArea.selectionEnd = end+ textArea.value.length; 
 }
 
 function enterAction(){
+    console.log('enter action')
+}
 
+function toggleCapsLock(item){
+
+    if (item.getAttribute('id') == 'off'){
+        item.setAttribute('id','on');
+    }else{
+        item.setAttribute('id','off');
+        
+    }
+    const characterElements = document.querySelectorAll('.char')
+    // console.log(characterElements);
+    for(let element of characterElements){
+    
+        console.log(element.innerText)
+        if (item.getAttribute('id') == 'on'){
+            element.innerText =  element.innerText.toUpperCase();
+            item.style.backgroundColor = 'green';
+        }else{
+            element.innerText =  element.innerText.toLowerCase();
+            item.style.backgroundColor = 'white';
+        }
+    }
 }
